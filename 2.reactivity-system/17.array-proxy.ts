f797369dde010b43455c0ecbc4856937041293e4
Louis Xu <xlzgogogo@foxmail.com>
2022-05-12 22:37:21 +0800

    arrary proxy

--- 2.reactivity-system/17.array-proxy.ts
+++ 2.reactivity-system/17.array-proxy.ts
@@ -1,8 +1,8 @@
-// 之前写的reactive函数都是浅响应（shallow reactive）
-// 因为只代理了外面一层，里面的对象都是普通对象
-// 解决办法非常简单，就是递归
-// 最好的办法是把核心逻辑收拢在一个createReactive函数，同事创建两个工具函数来根据需要返回深浅响应的对象
-// 同时基于createReactive，可以提供只读和浅只读的功能
+// js中数组是一种特殊的对象（异质对象）
+// 跟普通对象相比，只有[[DefineOwnProperty]]这个内部方法的实现不同
+// 因此大部分代理普通对象的操作可以直接使用，比如arr[0] = 'xxx'
+// 但是依然存在一些数组跟普通对象不同的操作方式
+//
 
 // 副作用函数容器
 const effectsBuckets = new WeakMap<
@@ -86,13 +86,14 @@ function createReactive<T extends object>(
     // ownKeys用于拦截for...in循环中的对象属性读取操作，包括新增和修改
     ownKeys(target) {
       // 将副作用函数与ITERATE_KEY进行关联
-      track(target, ITERATE_KEY)
+      //! 如果目标是数组，则使用length属性作为key建立联系，用于for-in和for-of循环的track
+      track(target, Array.isArray(target) ? 'length' : ITERATE_KEY)
 
       return Reflect.ownKeys(target)
     },
     // deleteProperty用于拦截对象的delete操作
     deleteProperty(target, key) {
-      //! 如果是只读属性，则给出警告直接返回
+      // 如果是只读属性，则给出警告直接返回
       if (isReadonly) {
         console.warn('set failed: readonly')
         return true
@@ -118,21 +119,22 @@ function createReactive<T extends object>(
       }
 
       // 非只读的时候才需要建立响应式联系
-      if (!isReadonly) {
+      //! 新增key为symbol的判断，因为当调用对象的迭代器时会读取Symbol.iterator属性，不应该追踪这个值
+      if (!isReadonly && typeof key !== 'symbol') {
         track(target, key)
       }
 
-      //! 用res缓存getter结果
+      // 用res缓存getter结果
       const res = Reflect.get(target, key, receiver)
 
-      //! 如果是浅响应则跟之前一样直接返回
+      // 如果是浅响应则跟之前一样直接返回
       if (isShallow) {
         return res
       }
 
-      //! 如果res结果是一个对象，且不等于null，则进行递归操作
+      // 如果res结果是一个对象，且不等于null，则进行递归操作
       if (typeof res === 'object' && res !== null) {
-        //! 如果数据为只读（且非浅只读），则调用readonly函数进行包装，否则调用reactive
+        // 如果数据为只读（且非浅只读），则调用readonly函数进行包装，否则调用reactive
         return isReadonly ? readonly(res) : reactive(res)
       }
 
@@ -140,7 +142,7 @@ function createReactive<T extends object>(
     },
     // 拦截设置操作
     set(target, key, newVal, receiver) {
-      //! 如果是只读属性，则给出警告直接返回
+      // 如果是只读属性，则给出警告直接返回
       if (isReadonly) {
         console.warn('set failed: readonly')
         return true
@@ -150,7 +152,12 @@ function createReactive<T extends object>(
       const oldValue = target[key]
 
       // 判断是新增属性还是修改属性
-      const type = Object.prototype.hasOwnProperty.call(target, key)
+      //! 新增数组判断逻辑
+      const type = Array.isArray(target)
+        ? Number(key) < target.length // 判断数值是新增还是修改操作
+          ? TriggerType.set
+          : TriggerType.add
+        : Object.prototype.hasOwnProperty.call(target, key)
         ? TriggerType.set
         : TriggerType.add
 
@@ -163,7 +170,8 @@ function createReactive<T extends object>(
           oldValue !== newVal &&
           (oldValue === oldValue || newVal === newVal)
         ) {
-          trigger(target, key, type)
+          //! 新增newVal参数，用于后续trigger的判断
+          trigger(target, key, type, newVal)
         }
       }
 
@@ -193,7 +201,12 @@ export function track(target, key) {
   activeEffect.deps.push(deps)
 }
 
-export function trigger(target, key, type: TriggerType = TriggerType.add) {
+export function trigger(
+  target,
+  key,
+  type: TriggerType = TriggerType.add,
+  newVal?
+) {
   const depsMap = effectsBuckets.get(target)
 
   if (!depsMap) return
@@ -211,6 +224,34 @@ export function trigger(target, key, type: TriggerType = TriggerType.add) {
       effectsToRun.add(fn)
     }
   })
+
+  const isArrayTarget = Array.isArray(target)
+
+  //! 当操作类型为新增并且目标对象是数组时，应该收集便于后续执行与length有关的副作用函数
+  if (isArrayTarget && type === TriggerType.add) {
+    const lengthEffects = depsMap.get('length')
+
+    lengthEffects?.forEach((fn) => {
+      if (fn !== activeEffect) {
+        effectsToRun.add(fn)
+      }
+    })
+  }
+
+  //! 如果操作的key是length，无论哪种类型，都需要收集便于后续执行索引值大于等于length的副作用函数
+  if (isArrayTarget && key === 'length') {
+    // 对于索引大于或者等于新length值的元素
+    // 需要把所有相关联的副作用函数去除并添加执行
+    depsMap.forEach((effects, key) => {
+      if (key >= newVal) {
+        effects.forEach((fn) => {
+          if (fn !== activeEffect) {
+            effectsToRun.add(fn)
+          }
+        })
+      }
+    })
+  }
 
   // 只有在新增或者删除类型时才需要触发，避免不必要的性能损耗
   if (type === TriggerType.add || type === TriggerType.delete) {
@@ -234,59 +275,40 @@ export function trigger(target, key, type: TriggerType = TriggerType.add) {
   })
 }
 
-//! 深响应函数
+// 深响应函数
 export function reactive<T extends object>(target: T): T {
   return createReactive(target)
 }
 
-//! 浅响应函数
+// 浅响应函数
 export function shallowReactive<T extends object>(target: T): T {
   return createReactive(target, true)
 }
 
-//! 深只读函数
+// 深只读函数
 export function readonly<T extends object>(target: T): T {
   return createReactive(target, false, true)
 }
 
-//! 浅只读函数
+// 浅只读函数
 export function shallowReadonly<T extends object>(target: T): T {
   return createReactive(target, true, true)
 }
 
-const originChild = {}
+const arr = reactive([1, 2, 3])
 
-const originParent = {
-  bar: 1,
-  foo: {
-    aaa: 5,
-  },
-}
-
-const proxyChild = reactive(originChild)
-
-const proxyParent = reactive(originParent)
-
-const demoReadonly = readonly(proxyParent)
-
-const demoShallowReadonly = shallowReadonly(proxyParent)
-
-demoShallowReadonly.bar = 3 // set failed: readonly
-
-demoReadonly.foo.aaa = 10 // set failed: readonly
-
-//! 使用proxyParent作为proxyChild的原型
-Object.setPrototypeOf(proxyChild, proxyParent)
-
-// rerender demo
 effect(() => {
-  // before: 1, 2, 2
-  // after: 1, 2
-  console.log((proxyChild as any).bar)
+  console.log(arr.length)
 })
 
-// 跟之前表现一致
-// @ts-ignore
-proxyChild.bar++ // before: 引发2次相同的副作用函数
+effect(() => {
+  for (const key in arr) {
+    console.log(key, ':', arr[key])
+  }
+})
+
+arr[4] = 100
+
+arr.length = 0
 
 export {}
